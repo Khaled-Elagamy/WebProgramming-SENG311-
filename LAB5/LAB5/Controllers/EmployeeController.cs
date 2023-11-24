5d0b2933ebc0fef310e293752996623cf61142dc
Khaled Elagamy <khaled_yasser1234@yahoo.com>
2023-11-24 15:57:03 +0300

    Add Lab5 Initail files

---
+++ LAB5/LAB5/Controllers/EmployeeController.cs
@@ -0,0 +1,95 @@
+﻿using LAB5.Data;
+using LAB5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LAB5.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public EmployeeController(IWebHostEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Layout = "_Lab2Layout";
+            using var context = new EmployeeContext();
+            var employees = context.Employees.ToList();
+            return View(employees);
+        }
+        public IActionResult Details(int id = 1)
+        {
+            ViewBag.Layout = "_Lab2Layout";
+            using var context = new EmployeeContext();
+            var employees = context.Employees.FirstOrDefault(m => m.Id == id);
+            return View(employees);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using var context = new EmployeeContext();
+            var employee = context.Employees.FirstOrDefault(m => m.Id == id);
+            return View(employee);
+        }
+        [HttpPost("[controller]/[action]/{id}")]
+        public IActionResult Edit(int id, Employee updatedEmployee)
+        {
+            if (ModelState.IsValid)
+            {
+                using var context = new EmployeeContext();
+                context.Employees.Update(updatedEmployee);
+                context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            ModelState.AddModelError("", "Something went wrong");
+            return View(updatedEmployee);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost("[action]/[controller]")]
+        public async Task<IActionResult> Create(Employee employee, IFormFile image)
+        {
+            if (ModelState.IsValid)
+            {
+                using var context = new EmployeeContext();
+
+                if (image != null && image.Length > 0)
+                {
+                    // Generate a unique filename
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + image.FileName;
+
+                    // Combine the filename with the wwwroot path where to store the images
+                    string filePath = Path.Combine(_hostingEnvironment.WebRootPath, "images", uniqueFileName);
+
+                    try
+                    {
+                        using (var fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await image.CopyToAsync(fileStream);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        ModelState.AddModelError("", "Something went wrong");
+                    }
+                    employee.Image = "/images/" + uniqueFileName;
+                }
+                context.Employees.Add(employee);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Something went wrong");
+            return View(employee);
+        }
+
+    }
+}
---
+++ LAB5/LAB5/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LAB5.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Surname { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Position { get; set; }
+        public string Image { get; set; }
+    }
+
+}
